0c3425fffe1e41b9e3e2e24b6e7f9bac5bfc746e
Ana Milea <mileaana14@stud.ase.ro>
2019-05-30 01:31:46 +0300

    test server3

---
+++ server.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const http = require('http');
+const { Client } = require('pg');
+
+var connectionString = "postgres://velypjqqksivzw:053bae585a9fc031019c0616372ab12114a20fdfc84f9395ef000f37cd9f5414@ec2-54-247-96-169.eu-west-1.compute.amazonaws.com:5432/ddutuvr3f1jsra";
+const client = new Client({
+    connectionString: connectionString, ssl: true
+});
+async function connect(){
+  await client.connect();
+}
+connect();
+app.use(bodyParser.json());
+
+var distDir = __dirname + "/dist";
+app.use(express.static(distDir));
+
+app.set('port', process.env.PORT || 5000);
+app.get('/api/users', function (req, res, next) {
+    client.query('SELECT * FROM Users where id = $1', [1],function (err, result) {
+        if (err) {
+            console.log(err);
+            res.status(400).send(err);
+        }
+        res.status(200).send(result.rows);
+    });
+});
+app.listen(process.env.PORT || 5000, function () {
+    console.log('Server is running.. on Port 4000');
+});
+
