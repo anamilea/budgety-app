638cb4630be8cf2de0b844a71e60ef9e5f53d545
Ana Milea <mileaana14@stud.ase.ro>
2019-05-31 12:54:58 +0300

    4

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const api = require('./server/routes.js');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, './dist/dizertatie-app')));
+app.use(express.static(path.join(__dirname, './dist/')));
 
 // Set our api routes
 app.use('/api', api);
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname ,'dist/dizertatie-app/index.html'));
+    res.sendFile(path.join(__dirname ,'dist/index.html'));
 });
 
 const port = process.env.PORT || '5000';
