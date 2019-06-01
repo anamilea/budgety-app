8ca259e818c6b2c1db47818b4007b69d20cb05b2
Ana Milea <mileaana14@stud.ase.ro>
2019-06-01 13:37:36 +0300

    remove mail from router

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { Client } = require('pg');
-const formidable = require('formidable');
+// const formidable = require('formidable');
 
 var connectionString = "postgres://velypjqqksivzw:053bae585a9fc031019c0616372ab12114a20fdfc84f9395ef000f37cd9f5414@ec2-54-247-96-169.eu-west-1.compute.amazonaws.com:5432/ddutuvr3f1jsra";
 const client = new Client({
@@ -17,16 +17,16 @@ router.get('/xyz', (req, res) => {
   res.send('api works');
 });
 
-router.get('/incoming', (req, res) => {
-  var form = new formidable.IncomingForm()
-  form.parse(req, function(err, fields, files) {
-    console.log(fields.attachments['0'])
-    console.log(fields.attachments['0']['file_name'])
-    console.log(fields.attachments['0']['url'])
-    res.writeHead(200, {'content-type': 'text/plain'})
-    res.send('Message Received. Thanks!\r\n')
-  });
-});
+// router.get('/incoming', (req, res) => {
+//   var form = new formidable.IncomingForm()
+//   form.parse(req, function(err, fields, files) {
+//     console.log(fields.attachments['0'])
+//     console.log(fields.attachments['0']['file_name'])
+//     console.log(fields.attachments['0']['url'])
+//     res.writeHead(200, {'content-type': 'text/plain'})
+//     res.send('Message Received. Thanks!\r\n')
+//   });
+// });
 
 router.get('/users', function (req, res, next) {
     client.query('SELECT * FROM Users where id = $1', [1],function (err, result) {
